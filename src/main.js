c4ef650f2929e69f915b91047f8c39c8db23ceb4
Jason Son <jasonson2004@gmail.com>
2025-01-24 23:28:21 -0800

    Initial commit

--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@
 
 // Spritesheet by ElvGames: https://elv-games.itch.io/free-fantasy-dreamland-sprites
 
-"use strict"
+"use strict";
 
 let config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 800,
-    render: {
-        pixelArt: true
+  type: Phaser.AUTO,
+  width: 800,
+  height: 800,
+  render: {
+    pixelArt: true,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
     },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: true,
-        }
-    },
-    scene: [ Movement ]
-}
+  },
+  scene: [Movement],
+};
 
-let game = new Phaser.Game(config)
+let game = new Phaser.Game(config);
 
-let cursors
-let { height, width } = game.config
+let cursors;
+let { height, width } = game.config;
